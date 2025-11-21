b632ed1aad1913a18b0ef6e964ebb6cf76edf9b8
alison2023xk <132129344+alison2023xk@users.noreply.github.com>
2025-11-21 15:17:46 +0300

    Update main.py

--- wb-smartops/main.py
+++ wb-smartops/main.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from dashboard.home import render_home
-from dashboard.sku_page import render_sku
-from dashboard.search_page import render_search
+from wb_smartops.dashboard.sku_page import render_sku
+from wb_smartops.dashboard.search_page import render_search
+from wb_smartops.dashboard.home import render_home
+
 
 st.set_page_config(
     page_title="WB SmartOps",
