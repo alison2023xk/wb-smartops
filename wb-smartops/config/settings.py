28db342539e1f1180f703955ab53025d376886e0
alison2023xk <132129344+alison2023xk@users.noreply.github.com>
2025-11-21 15:14:01 +0300

    Update settings.py

--- wb-smartops/config/settings.py
+++ wb-smartops/config/settings.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 WB_API_TOKEN = st.secrets["WB_API_TOKEN"]
-API_BASE = 'https://seller-analytics-api.wildberries.ru'
-DB_URL = 'sqlite:///wb_smartops.db'
+API_BASE = "https://seller-analytics-api.wildberries.ru"
+DB_URL = "sqlite:///wb_smartops.db"
